13e6f525c5a1186ca7f5c3b40aa18bc278014bdc
pomupekun <pomupekun@gmail.com>
2017-02-05 21:45:16 +0900

    set_conf.sh: changed default console to ubuntu

--- set_conf.sh
+++ set_conf.sh
@@ -4,5 +4,5 @@ sudo cat /var/lib/rancher/conf/cloud-config.yml > ./cloud-config_backup.yml
 sudo wget --no-check-certificate "https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub"
 sudo ros config set rancher.docker.tls true
 sudo ros config set rancher.network.dns.nameservers "['8.8.8.8','8.8.4.4']"
+sudo ros config set rancher.console ubuntu
 sudo ros config set ssh_authorized_keys ["`(cat vagrant.pub)`"]
-
